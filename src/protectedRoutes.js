a53b7fea92c5614d1bb96689119d493610f04195
mani-revanth <104350663+mani-revanth@users.noreply.github.com>
2023-05-12 11:16:43 +0530

    Update protectedRoutes.js

--- src/protectedRoutes.js
+++ src/protectedRoutes.js
@@ -3,14 +3,10 @@ import ReactDom from 'react-dom';
 import { useNavigate } from 'react-router';
 
 
-export default function ProtectedRoutes(children){
+export default function ProtectedRoutes({children}){
     const navigate=useNavigate(null);
     if(localStorage.getItem("user_using_is"))
-    return (
-        <>
-            {children}
-        </>
-    )
+    return children;
     else
-    window.location.href="/";
+    navigate("/");
 }
